0b542158ce0edfb3ec4dafd0881129d6c9787e11
thembones79 <michal.szumnarski@gmail.com>
2026-02-20 16:50:16 +0100

    Flappy Dragon - work in progress

--- hello_bterm/src/main.rs
+++ hello_bterm/src/main.rs
@@ -27,6 +27,8 @@ impl State {
     fn main_menu(&mut self, ctx: &mut BTerm){
         ctx.cls();
         ctx.print_centered(5,"Welcome to Flappy Dragon");
+        ctx.print_centered(8,"(P) Play Game");
+        ctx.print_centered(9,"(Q) Quit Game");
     }
 }
 
