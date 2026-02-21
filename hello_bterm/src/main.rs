8d06493afa11e242859054b167724bc88ff6023b
thembones79 <michal.szumnarski@gmail.com>
2026-02-21 19:19:01 +0100

    Flappy Dragon - work in progress

--- hello_bterm/src/main.rs
+++ hello_bterm/src/main.rs
@@ -16,19 +16,41 @@ impl State {
             mode: GameMode::Menu,
         }
     }
-    fn play(&mut self, ctx: &mut BTerm){
+    fn play(&mut self, ctx: &mut BTerm) {
         //TODO: Fill later this stub
 
         self.mode = GameMode::End;
     }
-    fn restart(&mut self){
-        self.mode=GameMode::Playing;
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
     }
-    fn main_menu(&mut self, ctx: &mut BTerm){
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(5,"Welcome to Flappy Dragon");
-        ctx.print_centered(8,"(P) Play Game");
-        ctx.print_centered(9,"(Q) Quit Game");
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 }
 
