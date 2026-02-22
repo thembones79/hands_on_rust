56eef022f2a013e1d9184d70a748a234e835b263
thembones79 <michal.szumnarski@gmail.com>
2026-02-22 09:42:09 +0100

    Flappy Dragon - work in progress

--- hello_bterm/src/main.rs
+++ hello_bterm/src/main.rs
@@ -6,14 +6,54 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
     mode: GameMode,
+    frame_time: f32,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
+            frame_time: 0.0,
+            player: Player::new(5, 25),
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
@@ -23,6 +63,8 @@ impl State {
     }
     fn restart(&mut self) {
         self.mode = GameMode::Playing;
+        self.frame_time = 0.0;
+        self.player = Player::new(5, 25);
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
