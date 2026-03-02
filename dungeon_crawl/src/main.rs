4c5b1d9db2e1a9c0bfb3a659cd1fa12ec7a0bc53
thembones79 <michal.szumnarski@gmail.com>
2026-03-02 13:46:56 +0100

    Dungeon Crawler - work in  progress

--- dungeon_crawl/src/main.rs
+++ dungeon_crawl/src/main.rs
@@ -7,6 +7,30 @@ mod prelude {
     pub use crate::map::*;
 }
 
-fn main() {
-    println!("Hello, world!");
+use prelude::*;
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
 }
