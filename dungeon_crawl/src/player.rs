43d8d7be604fe6153af1c1f69c1f88e2eba8b89d
thembones79 <michal.szumnarski@gmail.com>
2026-03-15 09:11:54 +0100

    Dungeon Crawler - work in  progress

---
+++ dungeon_crawl/src/player.rs
@@ -0,0 +1,20 @@
+use crate::prelude::*;
+
+pub struct Player {
+    pub position: Point,
+}
+
+impl Player {
+    pub fn new(position: Point) -> Self {
+        Self { position }
+    }
+    pub fn render(&self, ctx: &mut BTerm) {
+        ctx.set(
+            self.position.x,
+            self.position.y,
+            WHITE,
+            BLACK,
+            to_cp437('@'),
+        );
+    }
+}
