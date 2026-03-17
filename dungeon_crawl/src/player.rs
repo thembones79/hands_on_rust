0cf97e8fd4a4f0c8d7905f0e98c9a77f96f83fea
thembones79 <michal.szumnarski@gmail.com>
2026-03-17 16:55:13 +0100

    Dungeon Crawler - work in  progress

--- dungeon_crawl/src/player.rs
+++ dungeon_crawl/src/player.rs
@@ -26,6 +26,11 @@ impl Player {
                 VirtualKeyCode::Down => Point::new(0, 1),
                 _ => Point::zero(),
             };
+
+            let new_position = self.position + delta;
+            if map.can_enter_tile(new_position) {
+                self.position = new_position;
+            }
         }
     }
 }
