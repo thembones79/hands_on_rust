2cf5df0c54bb887cae88d099ed12ff8dc0693cff
thembones79 <michal.szumnarski@gmail.com>
2026-04-12 09:42:53 +0200

    Dungeon Crawler - work in  progress

--- dungeon_crawl/src/map_builder.rs
+++ dungeon_crawl/src/map_builder.rs
@@ -21,6 +21,20 @@ impl MapBuilder {
                 rng.range(2, 10),
             );
             let mut overlap = false;
+            for r in self.rooms.iter() {
+                if r.intersect(&room) {
+                    overlap = true;
+                }
+            }
+            if !overlap {
+                room.for_each(|p| {
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
+                        let idx = map_idx(p.x, p.y);
+                        self.map.tiles[idx] = TileType::Floor;
+                    }
+                });
+                self.rooms.push(room)
+            }
         }
     }
 }
