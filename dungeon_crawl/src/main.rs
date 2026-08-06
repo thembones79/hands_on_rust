257dd5b4c3b36625f3571445c85dac1e0673c737
thembones79 <michal.szumnarski@gmail.com>
2026-08-06 20:10:33 +0200

    Dungeon Crawler - map builder

--- dungeon_crawl/src/main.rs
+++ dungeon_crawl/src/main.rs
@@ -20,9 +20,11 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
@@ -43,13 +45,4 @@ fn main() -> BError {
         .build()?;
 
     main_loop(context, State::new())
-}
-
-fn new() -> Self {
-    let mut rng = RandomNumberGenerator::new();
-    let map_builder = MapBuilder::new(&mut rng);
-    Self {
-        map: map_builder.map,
-        player: Player::new(map_builder.player_start),
-    }
 }
