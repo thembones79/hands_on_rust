2812ce167f5fc070eaec79adb97889338c7ffb59
thembones79 <michal.szumnarski@gmail.com>
2026-08-23 11:59:01 +0200

    Dungeon Crawler - starting with graphics

--- dungeon_crawl/src/main.rs
+++ dungeon_crawl/src/main.rs
@@ -49,6 +49,8 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
