002e0ec0b87f318df4d3efe22c643f1c751b5777
thembones79 <michal.szumnarski@gmail.com>
2026-09-15 17:03:01 +0200

    Dungeon Crawler - starting with camera

--- dungeon_crawl/src/camera.rs
+++ dungeon_crawl/src/camera.rs
@@ -6,3 +6,13 @@ pub struct Camera {
     pub top_y: i32,
     pub bottom_y: i32,
 }
+
+impl Camera {
+    pub fn new(player_position: Point) -> Self {
+        Self{
+            left_x: player_position.x - DISPLAY_WIDTH/2,
+            right_x: player_position.x + DISPLAY_WIDTH/2,
+            top_y: player_position.x - DISPLAY_WIDTH/2,
+        }
+    }
+}
