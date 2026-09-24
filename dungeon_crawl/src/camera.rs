40ac2c2efea9944b88299a6d8603a82b627a458f
thembones79 <michal.szumnarski@gmail.com>
2026-09-24 19:51:38 +0200

    Dungeon Crawler - starting with camera

--- dungeon_crawl/src/camera.rs
+++ dungeon_crawl/src/camera.rs
@@ -12,7 +12,8 @@ impl Camera {
         Self{
             left_x: player_position.x - DISPLAY_WIDTH/2,
             right_x: player_position.x + DISPLAY_WIDTH/2,
-            top_y: player_position.x - DISPLAY_WIDTH/2,
+            top_y: player_position.x - DISPLAY_HEIGHT/2,
+            bottom_y: player_position.y - DISPLAY_HEIGHT/2,
         }
     }
 }
